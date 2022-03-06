d20416714f46eed54271a6912c1877143f595a2a
EC2 Default User <ec2-user@ip-172-31-26-200.ec2.internal>
2022-03-06 11:03:22 +0000

    S3Test18

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   //const [error, setError] = useState(null);
   //const [isLoaded, setIsLoaded] = useState(false);
   // const [items, setItems] = useState([]);
+  const [data, setData] = useState(null);
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  //useEffect(() => {
-  //  fetch(`https://b05slmqz8d.execute-api.us-east-1.amazonaws.com/test/`)
-  //    .then(res => res.json())
-  //    .then(
-  //      (result) => {
-          //setIsLoaded(true);
-          //setItems(result);
-  //      },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        //(error) => {
-        //  setIsLoaded(true);
-        //  setError(error);
-        //}
-  //    )
-  //}, [
     useEffect(() => {
 	fetch(`https://b05slmqz8d.execute-api.us-east-1.amazonaws.com/test/`)
-	.then((response) => console.log(response.json()));
+	.then((response) => console.log(response.json()))
+        .then((actualData) => {
+	        setData(actualData);
+	      })
     }, []);
 	 //
     return (
@@ -39,7 +23,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload from Github Actions with React2.
-        </p>
+	    {data.body}
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
