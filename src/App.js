c879e06d321123fc4c11f0aa56b02bf0452b0a36
EC2 Default User <ec2-user@ip-172-31-26-200.ec2.internal>
2022-03-06 12:09:15 +0000

    S3Test23

--- src/App.js
+++ src/App.js
@@ -11,10 +11,13 @@ function App() {
 
     useEffect(() => {
 	fetch(`https://b05slmqz8d.execute-api.us-east-1.amazonaws.com/test/`)
-	.then((response) => console.log(response.json()))
+	.then((response) => {
+		console.log(response.json());
+	    	return response.json();
+	})
         .then((actualData) => {
 	        setData(actualData);
-		console.log(actualData);
+		console.log(actualData.body);
 	      })
     }, []);
 	 //
